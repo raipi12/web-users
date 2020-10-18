1aad391cc3685c83a31f6c7f89d065890b89a748
Dumitras Panainte <dimpanainte2@gmail.com>
2020-10-18 18:36:29 +0300

    I confiI confi

---
+++ src/main/java/com/user/springbootdemo/service/UserService.java
@@ -0,0 +1,34 @@
+package com.user.springbootdemo.service;
+
+import com.user.springbootdemo.model.User;
+import com.user.springbootdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id){
+        return userRepository.getOne(id);
+    }
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+    public void deleteById(Long id){
+        userRepository.deleteById(id);
+
+    }
+}
